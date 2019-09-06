8afb28cf834fa9fd568dfa1dd58040ccbfc10ee3
Craig S. Cottingham <craig@cottingham.net>
2019-09-06 18:01:28 -0500

    Add metadata for hex.pm

--- mix.exs
+++ mix.exs
@@ -7,9 +7,15 @@ defmodule PidController.MixProject do
     [
       app: :pid_controller,
       version: "0.1.0",
+      name: "PidController",
+      description: description(),
+      source_url: "http://github.com/CraigCottingham/pid_controller",
+      homepage_url: "http://github.com/CraigCottingham/pid_controller",
+      package: package(),
       elixir: "~> 1.9",
-      elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: true],
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [espec: :test],
       aliases: aliases(),
@@ -39,6 +45,17 @@ defmodule PidController.MixProject do
     ]
   end
 
+  defp description do
+    "A PID (proportional/integral/derivative) controller in Elixir."
+  end
+
   defp elixirc_paths(:test), do: ["lib", "spec/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/CraigCottingham/pid_controller"}
+    ]
+  end
 end
