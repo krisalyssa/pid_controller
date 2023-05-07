839a4cb2de0fd47f6fa5f62b37c4b35e1bd7c8cf
rentpath-plateng <idg@rent.com>
2023-05-07 16:18:13 -0500

    chore: update dependencies

--- mix.exs
+++ mix.exs
@@ -39,10 +39,10 @@ defmodule PidController.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
-      {:espec, "~> 1.8", only: :test},
-      {:ex_doc, "~> 0.21.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:espec, "~> 1.9", only: :test},
+      {:ex_doc, "~> 0.29.4", only: [:dev, :test], runtime: false}
     ]
   end
 
